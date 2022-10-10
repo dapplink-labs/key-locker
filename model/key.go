cf6114e024ff20a96845b73f1636e857ef11bb12
guoshijiang <guoshijiang2012@163.com>
2022-10-10 11:00:34 +0800

    config.yml

--- model/key.go
+++ model/key.go
@@ -7,6 +7,6 @@ import (
 type Key struct {
 	*gorm.Model
 	ID        uint64 `gorm:"primary_key; description:ID"                    json:"id"`
-	KeySecret string `gorm:"index;type:varchar(256);description:KeySecret"  json:"key_secret"`
+	KeySecret string `gorm:"index;type:varchar(256);description:KeySecret"  json:"key_secret"` // 私钥分片加密
 	KeyUuid   string `gorm:"type:varchar(256);description:KeyUuid"          json:"key_uuid"`
 }

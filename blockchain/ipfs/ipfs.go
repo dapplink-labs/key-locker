b0326e122d7c64a7c01a7f138c123ee0baf25b5c
guoshijiang <guoshijiang2012@163.com>
2022-10-15 21:35:03 +0800

    fix

--- blockchain/ipfs/ipfs.go
+++ blockchain/ipfs/ipfs.go
@@ -45,6 +45,8 @@ func (a *KeyAdaptor) GetSupportChain(req *keylocker.SupportChainReq) (*keylocker
 
 func (a *KeyAdaptor) GetSocialKey(req *keylocker.GetSocialKeyReq) (*keylocker.GetSocialKeyRep, error) {
 	// todo: 调用 IPFS 接口获取 Social Key
+	// 1. req.uuid 取到链上的存储 req.key 的文件，
+	// 2. 解密返回就行
 	return &keylocker.GetSocialKeyRep{
 		Code: common.ReturnCode_SUCCESS,
 		Msg:  "get ipfs social key success",
@@ -53,6 +55,9 @@ func (a *KeyAdaptor) GetSocialKey(req *keylocker.GetSocialKeyReq) (*keylocker.Ge
 
 func (a *KeyAdaptor) SetSocialKey(req *keylocker.SetSocialKeyReq) (*keylocker.SetSocialKeyRep, error) {
 	// todo: 调用 IPFS 接口上传 Social Key
+	// 1. 如果对应 uuid(req.uuid) 没有 rsa 密钥对，生成 rsa 密钥对，生成 RSA 密钥对处理，私钥，用用户密码(req.password)进行 AES 加密存储， 公钥明文存储, 如果有直接使用
+	// 2. 用 rsa 私钥对 key(req.key 是用户上传的一个私钥) 加密，加密 key 调用 ipfs 上传
+	// 3. 返回加密的 RSA 的私钥和明文的 RSA 公钥匙, 加密方式，IPFS 对应的 CID
 	return &keylocker.SetSocialKeyRep{
 		Code: common.ReturnCode_SUCCESS,
 		Msg:  "set ipfs social key success",
--- model/secret.go
+++ model/secret.go
@@ -6,6 +6,6 @@ type Secret struct {
 	*gorm.Model
 	ID      uint64 `gorm:"primary_key; description:ID"                    json:"id"`
 	KeyUuid string `gorm:"type:varchar(256);description:KeyUuid"          json:"key_uuid"`
-	RsaPriv string `gorm:"type:varchar(256);description:RsaPriv"          json:"rsa_priv"`
-	RsaPub  string `gorm:"type:varchar(256);description:RsaPub"           json:"rsa_pub"`
+	RsaPriv string `gorm:"type:varchar(2048);description:RsaPriv"         json:"rsa_priv"`
+	RsaPub  string `gorm:"type:varchar(2048);description:RsaPub"          json:"rsa_pub"`
 }
